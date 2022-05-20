c63ce43a4651ef06bf957f27fdfdad0299594dec
Prisha Singla <prishasingla@prishas-air.lan>
2022-05-19 19:33:35 -0500

    hardcoded credit card data in data file

--- credit-card-recommender/src/data/data.js
+++ credit-card-recommender/src/data/data.js
@@ -1,17 +1,103 @@
-export const data = [
+export const cashbackCards = [
     {
-        img: "https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
-        name: "Dune",
-        genre: "Science Fiction"
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=185,width=300,quality=85/cdn/images/marketplace/credit_cards/84e1494c-bae2-4b0d-ac8d-e8c095ceb2fe/95f4b4eef72766c5ec9e6535530b866e39a210342ef0541cf276c9b8c0f6f01b.jpg",
+        name: "Chase Freedom Unlimited",
+        travelCashback: 6.5,
+        diningCashback: 4.5,
+        groceryCashback: 3,
+        streamingCashback: 3,
+        gasCashback: 3,
+        other: 1.5
     },
     {
-        img: "https://m.media-amazon.com/images/M/MV5BOTY2NzFjODctOWUzMC00MGZhLTlhNjMtM2Y2ODBiNGY1ZWRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
-        name: "Free Guy",
-        genre: "Comedy"
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=189,width=300,quality=85/cdn/images/marketplace/credit_cards/b954a46c-caec-11eb-abf1-efb90fc6e740/0de3b9f7172aa0043ac13d90f97a5a97ef447d1de9e2478654cfd432a9a6294e.jpg",
+        name: "Wells Fargo Active Cash",
+        travelCashback: 2,
+        diningCashback: 2,
+        groceryCashback: 2,
+        streamingCashback: 2,
+        gasCashback: 2,
+        other: 2
     },
     {
-        img: "https://m.media-amazon.com/images/M/MV5BMTExZmVjY2ItYTAzYi00MDdlLWFlOWItNTJhMDRjMzQ5ZGY0XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg",
-        name: "Eternals",
-        genre: "Science Fiction "
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=253,width=400,quality=85/cdn/images/marketplace/credit_cards/2681330d-450d-4f71-af38-3cb16f592c02/596f22094df609999878f43629debc8d416e18334ea8f5e68f9d92a72dd0b912.jpg",
+        name: "American Express Blue Cash Preferred",
+        travelCashback: 3,
+        diningCashback: 1,
+        groceryCashback: 6,
+        streamingCashback: 6,
+        gasCashback: 3,
+        other: 1
+    },
+    {
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=315,width=500,quality=85/cdn/images/marketplace/credit_cards/cfeca922-52db-4b18-be5e-8e4fe1448448/d0f43bdb9d3f1bd28db5d8416c6e497acd306df403dfdbac9c87b52f17d21324.jpg",
+        name: "Capital One SavorOne Cash Rewards",
+        travelCashback: 1,
+        diningCashback: 3,
+        groceryCashback: 3,
+        streamingCashback: 3,
+        gasCashback: 1,
+        other: 1
     }
 ]
+
+export const milesCards = [
+    {
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=315,width=500,quality=85/cdn/images/marketplace/credit_cards/1fc99a80-1508-4e14-83d7-eb5d5da65d97/3f7bbdd358bea4a71962b6e525795f0983b8c321f0ba8c1010303b9ecb020406.jpg",
+        name: "Capital One Venture Rewards",
+        travelMiles: 5,
+        hotelMiles: 5,
+        diningMiles: 2,
+        other: 2
+    },
+    {
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=304,width=480,quality=85/cdn/images/marketplace/credit_cards/fad86218-1dfc-430e-b85c-a2ce1cbfed90/0163bf7b5303500810b03612a6f19f7f32e7d6969e5bfb1423b0baba161a2443.jpg",
+        name: "Delta SkyMiles® Gold American Express",
+        travelMiles: 1,
+        hotelMiles: 1,
+        diningMiles: 2,
+        other: 1
+    }
+]
+
+export const pointsCards = [
+    {
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=185,width=300,quality=85/cdn/images/marketplace/credit_cards/7dce19da-5d96-4502-9fd2-70ff9655f13a/d82d2c88a684fb0591d07e208ecec733943643a4b700e03f880de204d257c790.jpg",
+        name: "Chase Sapphire Preferred",
+        travelPoints: 6.5,
+        diningPoints: 4.5,
+        groceryPoints: 3,
+        streamingPoints: 3,
+        gasPoints: 3,
+        other: 1.5
+    },
+    {
+        img: "https://www.nerdwallet.com/cdn-cgi/image/height=629,width=1000,quality=85/cdn/images/marketplace/credit_cards/2babb952-b115-11eb-ad15-dfead715694a/a43f567bb8efe50c95b3c6f4d5e519cda63eb3bc805e161cf57116d701dfcb82.jpg",
+        name: "U.S. Bank Altitude® Connect Visa Signature",
+        travelPoints: 4,
+        diningPoints: 2,
+        groceryPoints: 2,
+        streamingPoints: 2,
+        gasPoints: 4,
+        other: 1
+    }
+]
+
+// export const data = [
+//     {
+//         img: "https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
+//         name: "Dune",
+//         genre: "Science Fiction"
+//     },
+//     {
+//         img: "https://m.media-amazon.com/images/M/MV5BOTY2NzFjODctOWUzMC00MGZhLTlhNjMtM2Y2ODBiNGY1ZWRiXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+//         name: "Free Guy",
+//         genre: "Comedy"
+//     },
+//     {
+//         img: "https://m.media-amazon.com/images/M/MV5BMTExZmVjY2ItYTAzYi00MDdlLWFlOWItNTJhMDRjMzQ5ZGY0XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_.jpg",
+//         name: "Eternals",
+//         genre: "Science Fiction "
+//     }
+// ]
+
